1aad39c06461b7ec49a2b9102ad76d42be444f0a
Ngoc Huy <ngochuydev@gmail.com>
2016-03-29 01:23:20 +0700

    Add indexes to customer

--- db/structure.sql
+++ db/structure.sql
@@ -144,6 +144,27 @@ ALTER TABLE ONLY users
 
 
 --
+-- Name: customers_lower_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_email ON customers USING btree (lower((email)::text));
+
+
+--
+-- Name: customers_lower_first_name; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_first_name ON customers USING btree (lower((first_name)::text) varchar_pattern_ops);
+
+
+--
+-- Name: customers_lower_last_name; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_last_name ON customers USING btree (lower((last_name)::text) varchar_pattern_ops);
+
+
+--
 -- Name: index_customers_on_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -191,4 +212,6 @@ INSERT INTO schema_migrations (version) VALUES ('20160327041747');
 INSERT INTO schema_migrations (version) VALUES ('20160327051035');
 
 INSERT INTO schema_migrations (version) VALUES ('20160327070257');
+
+INSERT INTO schema_migrations (version) VALUES ('20160328180401');
 
