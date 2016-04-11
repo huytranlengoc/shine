45b65277f95f70f2fee4dfde30b1746340e5c3bd
Ngoc Huy <ngochuydev@gmail.com>
2016-04-12 06:59:57 +0700

    refactor by using angular router and templates

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,4 +1,9 @@
-var app = angular.module('customers', []);
+var app = angular.module('customers', 
+  [
+    'ngRoute',
+    'templates'
+  ]
+);
 
 app.controller("CustomerSearchController", [ "$scope", "$http",
   function($scope, $http) {
@@ -31,5 +36,16 @@ app.controller("CustomerSearchController", [ "$scope", "$http",
       page = page + 1;
       $scope.search($scope.keywords);
     }
+  }
+]);
+
+app.config([
+          "$routeProvider",
+  function($routeProvider) {
+
+    $routeProvider.when("/", {
+      controller: "CustomerSearchController",
+      templateUrl: "customer_search.html"
+    });
   }
 ]);
